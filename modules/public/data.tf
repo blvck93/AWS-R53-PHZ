72c1503eedcd912ce2bb286fcaf2f075f5cfe44f
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-05 16:08:44 +0100

    glupie bledy v75

--- modules/public/data.tf
+++ modules/public/data.tf
@@ -2,7 +2,7 @@ data "aws_vpc" "central_endpoint_vpc" {
   provider = aws.network_hub
   filter {
     name   = "tag:Name"
-    values = ["vpc-network-endpoints-${var.region}"]
+    values = ["test-vpc"]
   }
 }
 
