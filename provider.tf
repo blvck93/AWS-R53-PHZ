29652fc497a1a21cb4ee365e2ad5d9702f57c74f
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-05 16:20:52 +0100

    glupie bledy v80

--- provider.tf
+++ provider.tf
@@ -9,23 +9,9 @@ terraform {
 
 
 provider "aws" {
-  alias  = "network_hub_eu"
-  region = var.eu_region
-  default_tags {
-    tags = merge(var.tags, var.default_tags)
-  }
-  assume_role {
-    role_arn = var.role_arn
-  }
-}
-
-provider "aws" {
   alias  = "network_hub_us"
   region = var.us_region
   default_tags {
     tags = merge(var.tags, var.default_tags)
-  }
-  assume_role {
-    role_arn = var.role_arn
   }
 }
