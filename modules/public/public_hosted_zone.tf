fd5422617ae1576dd874026a79ad81ab5c9caff3
blvck93 <kamil.czarnocki@gmail.com>
2025-02-09 05:47:20 +0100

    Another day v2

--- modules/public/public_hosted_zone.tf
+++ modules/public/public_hosted_zone.tf
@@ -2,7 +2,7 @@ resource "aws_route53_zone" "public" {
   name = var.phz_name
   vpc {
     vpc_region = var.region
-    vpc_id     = data.aws_vpcs.central_endpoint_vpc.ids[0]
+    vpc_id     = data.aws_vpcs.central_endpoint_vpc.id
   }
   lifecycle {
     ignore_changes = [vpc]
