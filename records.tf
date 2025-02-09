87fb9609a033b9288d0bc95aef4074e4680ca467
blvck93 <kamil.czarnocki@gmail.com>
2025-02-09 07:22:44 +0100

    adding Coffee record

--- records.tf
+++ records.tf
@@ -26,3 +26,11 @@ resource "aws_route53_record" "blvck_ovh_cname" {
   ttl     = "300"
   records = ["google.com"]
 }
+
+resource "aws_route53_record" "coffee_blvck_ovh_a" {
+  zone_id = data.aws_route53_zone.blvck_ovh.zone_id
+  name    = "coffee.blvck.ovh"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["google.com"]
+}
