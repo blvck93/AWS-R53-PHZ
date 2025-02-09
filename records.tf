8fcefe9604cfdb5ce5a39bee2d86172578395348
blvck93 <kamil.czarnocki@gmail.com>
2025-02-09 07:40:26 +0100

    add records

---
+++ records.tf
@@ -0,0 +1,36 @@
+data "aws_route53_zone" "blvck_ovh" {
+    provider = aws.network_hub_us
+    name = "blvck.ovh"
+}
+
+resource "aws_route53_record" "blvck_ovh_a" {
+  zone_id = data.aws_route53_zone.blvck_ovh.zone_id
+  name    = "blvck.ovh"
+  type    = "A"
+  ttl     = "300"
+  records = ["8.8.8.8"]
+}
+
+resource "aws_route53_record" "example_blvck_ovh_a" {
+  zone_id = data.aws_route53_zone.blvck_ovh.zone_id
+  name    = "example.blvck.ovh"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["google.com"]
+}
+
+resource "aws_route53_record" "blvck_ovh_cname" {
+  zone_id = data.aws_route53_zone.blvck_ovh.zone_id
+  name    = "www.blvck.ovh"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["google.com"]
+}
+
+resource "aws_route53_record" "coffee_blvck_ovh_cname" {
+  zone_id = data.aws_route53_zone.blvck_ovh.zone_id
+  name    = "coffee.blvck.ovh"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["google.com"]
+}
