110b87f029284d96b9fba2fc45d4ff3329c46727
blvck93 <kamil.czarnocki@gmail.com>
2025-02-09 07:05:05 +0100

    adding records FINAL v2

--- records.tf
+++ records.tf
@@ -11,7 +11,7 @@ resource "aws_route53_record" "blvck_ovh_a" {
   records = ["8.8.8.8"]
 }
 
-resource "aws_route53_record" "example.blvck_ovh_a" {
+resource "aws_route53_record" "example_blvck_ovh_a" {
   zone_id = data.aws_route53_zone.blvck_ovh.zone_id
   name    = "example.blvck.ovh"
   type    = "A"
